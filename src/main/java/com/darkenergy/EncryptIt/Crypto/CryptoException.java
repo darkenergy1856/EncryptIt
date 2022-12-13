1d4197d96714cebda74793100b75377fb111d852
Aryan Chandra <aryanchandra7360@gmail.com>
2022-12-13 11:09:54 +0530

    Nothing Major

--- src/main/java/com/darkenergy/EncryptIt/Crypto/CryptoException.java
+++ src/main/java/com/darkenergy/EncryptIt/Crypto/CryptoException.java
@@ -1,14 +1,14 @@
 package com.darkenergy.EncryptIt.Crypto;
 
+import java.io.Serial;
+
 public class CryptoException extends Exception {
 
     /**
 	 *
 	 */
+    @Serial
 	private static final long serialVersionUID = 7734949223338730922L;
-
-	public CryptoException() {
-    }
 
     public CryptoException(String message, Throwable throwable) {
         super(message, throwable);
--- src/test/java/com/darkenergy/EncryptIt/EncryptItApplicationTests.java
+++ src/test/java/com/darkenergy/EncryptIt/EncryptItApplicationTests.java
@@ -3,6 +3,7 @@ package com.darkenergy.EncryptIt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SuppressWarnings("EmptyMethod")
 @SpringBootTest
 class EncryptItApplicationTests {
 
